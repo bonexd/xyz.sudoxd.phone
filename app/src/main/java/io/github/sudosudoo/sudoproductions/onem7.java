1bf74d1570b7f140d731502b3d6f68578667e014
sudosudoo <35059872+sudosudoo@users.noreply.github.com>
2018-06-09 19:20:20 +1000

    1.3.2-beta3
    Signed-off-by: sudosudoo <35059872+sudosudoo@users.noreply.github.com>

--- app/src/main/java/io/github/sudosudoo/sudoproductions/onem7.java
+++ app/src/main/java/io/github/sudosudoo/sudoproductions/onem7.java
@@ -1,7 +1,9 @@
 package io.github.sudosudoo.sudoproductions;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class onem7 extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class onem7 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onem7);
+    }
+    public void onem7rom(View v) {
+        startActivity(new Intent(this, onem7rom.class));
     }
 }
---
+++ app/src/main/java/io/github/sudosudoo/sudoproductions/onem7rom.java
@@ -0,0 +1,13 @@
+package io.github.sudosudoo.sudoproductions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class onem7rom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onem7rom);
+    }
+}
