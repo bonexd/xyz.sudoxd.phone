6279f384e267482aee554f7c715cdb67b965d7db
sudosudoo <35059872+sudosudoo@users.noreply.github.com>
2018-06-14 19:26:28 +1000

    add series page to sort phones even more
    Signed-off-by: sudosudoo <35059872+sudosudoo@users.noreply.github.com>

---
+++ app/src/main/java/io/github/sudosudoo/sudoproductions/htconeseries.java
@@ -0,0 +1,27 @@
+package io.github.sudosudoo.sudoproductions;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class htconeseries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_htconeseries);
+    }
+    public void onem7(View v) {
+        startActivity(new Intent(this, onem7.class));
+        if (Build.VERSION.SDK_INT >= 26) {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(30);
+        }
+
+    }
+}
