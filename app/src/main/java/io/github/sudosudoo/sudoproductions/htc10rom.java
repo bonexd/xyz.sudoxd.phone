4e51beb3990c1e6f28c48021efd9ea2c62ea6f2f
Jamie J. Ball <waytoosalty@gmail.com>
2018-09-24 12:20:08 +1000

    add htc 10 website and rom links
    Signed-off-by: Jamie J. Ball <waytoosalty@gmail.com>

---
+++ app/src/main/java/io/github/sudosudoo/sudoproductions/htc10rom.java
@@ -0,0 +1,13 @@
+package io.github.sudosudoo.sudoproductions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class htc10rom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_htc10rom);
+    }
+}
