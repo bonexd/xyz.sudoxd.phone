29bf5d1d6f4f32cecb464537e4fbeeee6f7395c3
xd <waytoosalty@gmail.com>
2019-01-27 11:47:51 +1100

    convert htc one m7 to kotlin

---
+++ app/src/main/java/io/github/sudoxd/sudophones/onem7romll.kt
@@ -0,0 +1,36 @@
+package io.github.sudoxd.sudophones
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+
+class onem7romll : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onem7romll)
+    }
+
+    fun goToOnem7llgpe(view: View) {
+        goToUrl()
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
+        }
+    }
+
+    private fun goToUrl() {
+        val uriUrl = Uri.parse("https://forum.xda-developers.com/htc-one/development/rom-google-play-edition-lollipop-v1-00-t2963927")
+        val launchBrowser = Intent(Intent.ACTION_VIEW, uriUrl)
+        startActivity(launchBrowser)
+    }
+
+}
