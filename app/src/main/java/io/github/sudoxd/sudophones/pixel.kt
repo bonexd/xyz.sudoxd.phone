97081333d35b42ace4014f7c71b7298141c75053
xd <waytoosalty@gmail.com>
2019-01-27 11:54:06 +1100

    convert google pixel to kotlin

---
+++ app/src/main/java/io/github/sudoxd/sudophones/pixel.kt
@@ -0,0 +1,43 @@
+package io.github.sudoxd.sudophones
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class pixel : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pixel)
+    }
+
+    private fun goToUrl(url: String) {
+        val uriUrl = Uri.parse(url)
+        val launchBrowser = Intent(Intent.ACTION_VIEW, uriUrl)
+        startActivity(launchBrowser)
+    }
+
+    fun gotopixelwebpage(view: View) {
+        goToUrl("https://www.android.com/intl/en_au/phones/pixel/")
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
+        }
+    }
+
+    fun pixelrom(v: View) {
+        startActivity(Intent(this, pixelrom::class.java))
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
+        }
+    }
+}
