56b704cefaeb19e9733b5162bfebfbb93be4644b
xd <waytoosalty@gmail.com>
2019-01-27 11:45:53 +1100

    convert htc 10 to kotlin

---
+++ app/src/main/java/io/github/sudoxd/sudophones/htc10rom.kt
@@ -0,0 +1,36 @@
+package io.github.sudoxd.sudophones
+
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class htc10rom : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_htc10rom)
+    }
+
+    fun htc10romnougat(v: View) {
+        startActivity(Intent(this, htc10romnougat::class.java))
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
+        }
+    }
+
+    fun htc10romoreo(v: View) {
+        startActivity(Intent(this, pixelromoreo::class.java))
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
+        }
+    }
+}
