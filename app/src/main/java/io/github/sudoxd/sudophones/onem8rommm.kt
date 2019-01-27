43526bd775ee8a68326542ce5a50ca8715566739
xd <waytoosalty@gmail.com>
2019-01-27 11:50:52 +1100

    convert htc one m8 to kotlin

---
+++ app/src/main/java/io/github/sudoxd/sudophones/onem8rommm.kt
@@ -0,0 +1,43 @@
+package io.github.sudoxd.sudophones
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class onem8rommm : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onem8rommm)
+    }
+
+    private fun goToUrl(url: String) {
+        val uriUrl = Uri.parse(url)
+        val launchBrowser = Intent(Intent.ACTION_VIEW, uriUrl)
+        startActivity(launchBrowser)
+    }
+
+    fun gotoonem8mmmiui(view: View) {
+        goToUrl("https://forum.xda-developers.com/htc-one-m8/development/stable-miui-8-v1cm-base-t3623922")
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
+        }
+    }
+
+    fun gotoonem8mmskydraggpe(view: View) {
+        goToUrl("https://forum.xda-developers.com/showthread.php?t=2706391")
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
+        }
+    }
+}
