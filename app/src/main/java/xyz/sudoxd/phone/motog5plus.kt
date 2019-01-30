3168c01ab165645e7eab2638c8d3cc7070fd4788
xd <waytoosalty@gmail.com>
2019-01-30 11:18:11 +1100

    add g5+ rom links and important info from issue #9

--- app/src/main/java/xyz/sudoxd/phone/motog5plus.kt
+++ app/src/main/java/xyz/sudoxd/phone/motog5plus.kt
@@ -40,4 +40,13 @@ class motog5plus : AppCompatActivity() {
             (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
         }
     }
+
+    fun mg5prompage(v: View) {
+        startActivity(Intent(this, motog5plusrom::class.java))
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
+        }
+    }
 }
--- app/src/main/java/xyz/sudoxd/phone/motog5plusrom.kt
+++ app/src/main/java/xyz/sudoxd/phone/motog5plusrom.kt
@@ -1,12 +1,27 @@
 package xyz.sudoxd.phone
 
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.view.View
 
 class motog5plusrom : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_motog5plusrom)
+    }
+
+    fun motog5plusromimportant(v: View) {
+        startActivity(Intent(this, motog5plusromimportant()::class.java))
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
+        }
     }
 }
