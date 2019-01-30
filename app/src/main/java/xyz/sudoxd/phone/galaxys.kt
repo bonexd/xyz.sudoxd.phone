37594512adf3882a9908eb8dfd21e4b09e0ebac5
xd <waytoosalty@gmail.com>
2019-01-30 17:46:56 +1100

    galaxy s

---
+++ app/src/main/java/xyz/sudoxd/phone/galaxys.kt
@@ -0,0 +1,12 @@
+package xyz.sudoxd.phone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class galaxys : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_galaxys)
+    }
+}
---
+++ app/src/main/java/xyz/sudoxd/phone/galaxysseries.kt
@@ -0,0 +1,27 @@
+package xyz.sudoxd.phone
+
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class galaxysseries : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_galaxysseries)
+    }
+
+    fun galaxys(v: View) {
+        startActivity(Intent(this, galaxys::class.java))
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
+        }
+    }
+}
