16f19269a8c3943ddeebfe9a3390d6b6fbcdbd02
xd <waytoosalty@gmail.com>
2019-04-16 22:39:42 +1000

    samsung: add galaxy s2

--- app/src/main/java/xyz/sudoxd/phone/galaxys2.kt
+++ app/src/main/java/xyz/sudoxd/phone/galaxys2.kt
@@ -2,31 +2,29 @@ package xyz.sudoxd.phone
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
-import android.os.VibrationEffect
-import android.os.Vibrator
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.View
 
-class galaxysseries : AppCompatActivity() {
+class galaxys2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_galaxysseries)
+        setContentView(R.layout.activity_galaxys2)
     }
 
-    fun galaxys(v: View) {
-        startActivity(Intent(this, galaxys::class.java))
-        if (Build.VERSION.SDK_INT >= 26) {
-            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
-        } else {
-            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
-        }
+    private fun goToUrl(url: String) {
+        val uriUrl = Uri.parse(url)
+        val launchBrowser = Intent(Intent.ACTION_VIEW, uriUrl)
+        startActivity(launchBrowser)
     }
 
-    fun galaxys2(v: View) {
-        startActivity(Intent(this, galaxys2::class.java))
+    fun gotogalaxyswebpage(view: View) {
+        goToUrl("https://web.archive.org/web/20110924103758/http://www.samsung.com/au/consumer/mobile-phone/mobile-phone/smartphone/GT-I9100LKJXSA/index.idx?pagetype=prd_detail&returnurl=")
         if (Build.VERSION.SDK_INT >= 26) {
             (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
         } else {
