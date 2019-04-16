075785e76247c94778f1833adf3a7606fadaa651
xd <waytoosalty@gmail.com>
2019-04-16 23:21:43 +1000

    samsung: add galaxy camera

--- app/src/main/java/xyz/sudoxd/phone/galaxycamera.kt
+++ app/src/main/java/xyz/sudoxd/phone/galaxycamera.kt
@@ -2,6 +2,7 @@ package xyz.sudoxd.phone
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -9,23 +10,21 @@ import android.os.VibrationEffect
 import android.os.Vibrator
 import android.view.View
 
-class samsungmain : AppCompatActivity() {
+class galaxycamera : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_samsungmain)
+        setContentView(R.layout.activity_galaxycamera)
     }
 
-    fun samsungsseries(v: View) {
-        startActivity(Intent(this, galaxysseries::class.java))
-        if (Build.VERSION.SDK_INT >= 26) {
-            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
-        } else {
-            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
-        }
+    private fun goToUrl(url: String) {
+        val uriUrl = Uri.parse(url)
+        val launchBrowser = Intent(Intent.ACTION_VIEW, uriUrl)
+        startActivity(launchBrowser)
     }
-    fun samsunggcseries(v: View) {
-        startActivity(Intent(this, galaxygcseries::class.java))
+
+    fun gotogalaxyswebpage(view: View) {
+        goToUrl("https://web.archive.org/web/20130802041411/http://www.samsung.com/au/consumer/mobile-phone/galaxy-camera/")
         if (Build.VERSION.SDK_INT >= 26) {
             (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
         } else {
--- app/src/main/java/xyz/sudoxd/phone/galaxygcseries.kt
+++ app/src/main/java/xyz/sudoxd/phone/galaxygcseries.kt
@@ -3,29 +3,38 @@ package xyz.sudoxd.phone
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 import android.view.View
 
-class samsungmain : AppCompatActivity() {
+class galaxygcseries : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_samsungmain)
+        setContentView(R.layout.activity_galaxygcseries)
     }
 
-    fun samsungsseries(v: View) {
-        startActivity(Intent(this, galaxysseries::class.java))
+    fun galaxycamera(v: View) {
+        startActivity(Intent(this, galaxycamera::class.java))
         if (Build.VERSION.SDK_INT >= 26) {
             (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
         } else {
             (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
         }
     }
-    fun samsunggcseries(v: View) {
-        startActivity(Intent(this, galaxygcseries::class.java))
+
+    fun galaxys2(v: View) {
+        startActivity(Intent(this, galaxys2::class.java))
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
+        }
+    }
+    fun galaxys3(v: View) {
+        startActivity(Intent(this, galaxys3::class.java))
         if (Build.VERSION.SDK_INT >= 26) {
             (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
         } else {
