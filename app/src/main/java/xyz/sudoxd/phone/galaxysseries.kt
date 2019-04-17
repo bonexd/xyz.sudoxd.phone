5e0aa29bed8f3d4929f1b189004219ae288422f8
xd <waytoosalty@gmail.com>
2019-04-17 19:40:40 +1000

    samsung: add galaxy s7

--- app/src/main/java/xyz/sudoxd/phone/galaxysseries.kt
+++ app/src/main/java/xyz/sudoxd/phone/galaxysseries.kt
@@ -64,4 +64,13 @@ class galaxysseries : AppCompatActivity() {
             (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator)
         }
     }
+
+    fun galaxys7(v: View) {
+        startActivity(Intent(this, galaxys7::class.java))
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator)
+        }
+    }
 }
