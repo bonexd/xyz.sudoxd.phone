5418195ee5210c77fc1ef607ce5292cb06af668e
xd <waytoosalty@gmail.com>
2019-04-18 00:03:49 +1000

    samsung: add s10/s10e

--- app/src/main/java/xyz/sudoxd/phone/galaxysseries.kt
+++ app/src/main/java/xyz/sudoxd/phone/galaxysseries.kt
@@ -90,4 +90,12 @@ class galaxysseries : AppCompatActivity() {
             (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator)
         }
     }
+    fun galaxys10(v: View) {
+        startActivity(Intent(this, galaxys10::class.java))
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator)
+        }
+    }
 }
