ea00d94d3a4c4fc5087ada6c4f84ea9ebd0d5130
xd <waytoosalty@gmail.com>
2019-05-25 00:00:27 +1000

    google: add pixel 2 and pixel 2 xl

--- app/src/main/java/xyz/sudoxd/phone/pixel2.kt
+++ app/src/main/java/xyz/sudoxd/phone/pixel2.kt
@@ -2,31 +2,31 @@ package xyz.sudoxd.phone
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import xyz.sudoxd.phone.R
 
-class pixelseries : AppCompatActivity() {
+class pixel2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pixelseries)
+        setContentView(R.layout.activity_pixel2)
     }
 
-    fun pixel(v: View) {
-        startActivity(Intent(this, pixel::class.java))
-        if (Build.VERSION.SDK_INT >= 26) {
-            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
-        } else {
-            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(30)
-        }
+    private fun goToUrl(url: String) {
+        val uriUrl = Uri.parse(url)
+        val launchBrowser = Intent(Intent.ACTION_VIEW, uriUrl)
+        startActivity(launchBrowser)
     }
 
-    fun pixel2(v: View) {
-        startActivity(Intent(this, pixel2::class.java))
+
+    fun gotopixel2webpage(view: View) {
+        goToUrl("https://web.archive.org/web/20171004230117/https://store.google.com/product/pixel_2")
         if (Build.VERSION.SDK_INT >= 26) {
             (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE))
         } else {
